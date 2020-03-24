37c58191e53057940d809b5b2e112168e3e7dec4
Pedro Prieto <pedro.prieto003@gmail.com>
2020-03-24 03:06:47 -0400

    5 rooms, enemy and attack

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    // PLAYER SPEED
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    // PLAYER POSITION TO CHANGE
+    private Vector3 change;
+    private Animator animator;
+    public PlayerState currentState;
+
+    public enum PlayerState
+    {
+        walk,
+        attack,
+        interact
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.walk;
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // RESET vector on every move
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking",true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.33f);
+        currentState = PlayerState.walk;
+         
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        change.Normalize();
+        // MOVE PLAYER
+        myRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+    }
+}
